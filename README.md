95826972e58b581c7bc5c2713845a14e6b379f9a
Samyra <samyramendesli@gmail.com>
2024-11-21 13:10:25 +0000

    up

---
+++ README.md
@@ -0,0 +1,151 @@
+# SAMYRA MENDES LIMA
+# UFCD 10790 - Projeto de Programação
+# FORMADOR - JOÃO PEDRO BARBOSA DAVID
+# Sistema Gestão DE TAREFAS
+
+Uma aplicação simples desenvolvida em Python para gerir tarefas, permite adicionar, visualizar, editar e remover tarefas.
+
+## Índice
+
+- [Introdução](#introdução)
+- [Âmbito do Projeto](#âmbito-do-projeto)
+- [Levantamento de Requisitos](#levantamento-de-requisitos)
+- [Elaboração do Projeto](#elaboração-do-projeto)
+- [Desempenho do Projeto](#desempenho-do-projeto)
+- [Como Executar o Projeto](#como-executar-o-projeto)
+- [Resultados](#resultados)
+- [Conclusão](#conclusão)
+- [Trabalhos Futuros](#trabalhos-futuros)
+
+## Introdução
+
+Este projeto consiste em um sistema básico de gestão de tarefas desenvolvido em Python. A aplicação permite 
+aos usuários adicionar, editar, excluir e visualizar tarefas. O objetivo principal é proporcionar uma ferramenta 
+simples e eficaz para organização de atividades diárias, promovendo maior produtividade e controle.
+
+## Âmbito do Projeto
+
+-**Objetivo**: 
+Desenvolver uma aplicação simples para ajudar os usuários a gerenciar suas tarefas diárias de forma eficiente.
+
+- **Objetivos Específicos**:
+  - Implementar funcionalidades CRUD (Adicionar, Ler, Atualizar, Eliminar).
+  - Utilizar uma base de dados simples (SQLite) para armazenamento de tarefas.
+
+- **Público-Alvo**: 
+Profissionais, estudantes e qualquer pessoa que necessite de uma ferramenta para organizar suas atividades 
+diárias, tarefas pessoais e compromissos.
+
+- **Limitações**:
+Devido ao tempo e ao nível de conhecimento da linguagem Python, o sistema não contará com recursos avançados, 
+como notificações ou prazos. A interface gráfica também será simples, com foco na funcionalidade.
+
+
+## Levantamento de Requisitos
+### Requisitos Funcionais
+
+- **RF01**: A aplicação deve permitir adicionar novas tarefas com uma descrição.
+- **RF02**: A aplicação deve permitir editar a descrição de tarefas existentes.
+- **RF03**: A aplicação deve permitir excluir tarefas que não são mais necessárias.
+- **RF04**: A aplicação deve permitir marcar tarefas como concluídas através de uma interface de checkbox.
+
+### Requisitos Não Funcionais
+
+- **RNF01**: A aplicação foi desenvolvida utilizando Python 3.x.
+- **RNF02**: A interface gráfica foi criada utilizando Tkinter.
+- **RNF03**: A aplicação utiliza SQLite para persistência de dados.
+
+
+## Desenvolvimento do Projeto
+### Arquitetura
+
+A arquitetura da aplicação é simples e dividida em duas partes principais:
+- Front-End: Interface gráfica desenvolvida com Tkinter, permitindo a interação com o usuário de forma intuitiva e prática.
+- Back-End: Lógica de negócios para processar as ações do usuário e interação com a base de dados SQLite.
+
+### Tecnologias Utilizadas
+
+**Linguagens**: Python 3.10
+**Bibliotecas**:
+  - `Tkinter`: Para a interface gráfica do usuário.
+  - `sqlite3`: Para a operação com a base de dados SQLite.
+**Ferramentas**:
+  - Visual Studio Code como IDE para desenvolvimento.
+
+### Implementação
+implementação foi realizada com foco nas funcionalidades CRUD, utilizando SQLite para armazenar as tarefas e 
+Tkinter para a interface gráfica. Cada tarefa possui uma descrição, e o usuário pode adicionar, editar, excluir 
+e marcar como concluída.
+
+
+## Desempenho do Projeto
+### Testes Realizados
+
+- **Testes de Inserção**: 
+    -Verificação da inserção de tarefas com diferentes descrições, garantindo que todas as 
+      tarefas sejam armazenadas corretamente na base de dados, utilizei prints para verificar 
+      os resultados, diretamente no terminal.
+- **Testes de Exclusão**: 
+    -Teste da exclusão de tarefas, verificando que as exclusões sejam refletidas tanto na 
+      interface quanto no banco de dados.
+- **Testes de Edição**: 
+    -Teste para garantir que a edição das tarefas funcione corretamente e que as alterações 
+      sejam salvas na base de dados.
+
+## Como Executar o Projeto
+
+```bash
+# Clonar o repositório do GitHub
+
+git clone https://github.com/Samyra180/Tarefas.git
+
+# Navegar até ao diretório onde se encontra o projeto
+cd nome-do-projeto
+
+# ativar o ambiente virtual
+   - source venv/bin/activate
+
+# Instalar as dependências
+   - pip install -r requirements.txt
+
+# Executar a applicação
+   - python main.py
+```
+
+
+## Resultados
+A aplicação foi desenvolvida com sucesso, atendendo aos requisitos funcionais e não funcionais definidos. A interface 
+gráfica foi criada com o Tkinter, proporcionando uma experiência de usuário intuitiva. A persistência de dados foi 
+realizada com o SQLite, garantindo que as tarefas adicionadas sejam salvas e possam ser recuperadas posteriormente.
+
+
+## Conclusão
+### Dificuldades Encontradas
+Enfrentei algumas dificuldades relacionadas ao aspecto gráfico do projeto. Além disso, tive uma certa dificuldade 
+com a linguagem Python, pois ainda não me sinto completamente à vontade com ela. Ainda assim, a experiência foi valiosa, 
+pois me permitiu aprender mais sobre Python e como lidar com erros.
+
+### Reflexões
+As dificuldades mencionadas anteriormente foram fundamentais para o meu aprendizado e contribuíram significativamente para 
+o desenvolvimento do projeto. Elas me ajudaram a ganhar mais confiança ao longo do processo, além de me proporcionar uma 
+compreensão mais profunda sobre Python. Esse processo de aprendizado foi essencial para o sucesso do projeto e para o meu 
+crescimento como desenvolvedor.
+
+
+## Trabalhos Futuros
+
+- **Adicionar funcionalidade de prazo**: Implementar a possibilidade de definir um prazo para cada tarefa, permitindo que 
+o usuário defina quando uma tarefa precisa ser concluída.
+- **Notificações**: Implementar notificações para lembrar o usuário das tarefas próximas do prazo ou com status pendente. 
+Isso pode ser feito utilizando bibliotecas como schedule ou integração com serviços de notificações externas.
+- **Melhorar o design gráfico**: Melhorar a interface do usuário, tornando-a mais atraente e intuitiva, com o uso de temas 
+personalizados, fontes mais legíveis e uma disposição mais agradável dos elementos.
+- **Adicionar categorias para tarefas**: Permitir que as tarefas sejam agrupadas por categorias (ex.: trabalho, pessoal, 
+estudos) para facilitar a organização e o gerenciamento.
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,160 @@
+import tkinter as tk
+from tkinter import messagebox, font
+import db  # Importa as funções do db.py
+import sqlite3
+
+
+# Função para criar a janela de abertura
+def janela_abertura():
+    # Criar a janela de abertura
+    splash = tk.Tk()
+    splash.geometry("510x500")  # Tamanho da janela de abertura
+    splash.title("Bem-vindo ao Organizador de Tarefas")
+
+    # Adiciona uma imagem ou texto à janela de abertura (pode ser uma imagem de logo)
+    label = tk.Label(splash, text="Carregando...", font=("Arial", 20))
+    label.pack(expand=True)
+
+    # Fechar a janela de abertura após alguns segundos
+    splash.after(3000, lambda: (splash.destroy(), janela_principal()))  # Fecha após 3 segundos e chama a janela principal
+
+    splash.mainloop()
+
+# Criar a janela principal
+def janela_principal():
+    janela = tk.Tk()
+    janela.iconbitmap("imgs/list.ico")  # Define o ícone da janela
+    janela.title("Organizador de tarefas")  # Define o título da janela
+    janela.configure(bg="#F0F0F0")  # Cor de fundo da janela
+    janela.geometry("510x500")  # Define o tamanho da janela
+    janela.resizable(False, False)  # Bloqueia o redimensionamento da janela
+
+    frame_em_edicao = None
+
+    # Função para adicionar tarefa
+    def adicionar_tarefa():
+        global frame_em_edicao
+
+        tarefa = entrada_tarefa.get().strip()
+        if tarefa and tarefa != "Escreva sua tarefa aqui":
+            if frame_em_edicao is not None:
+                atualizar_tarefa(tarefa)
+                frame_em_edicao = None
+            else:
+                db.adicionar_tarefa(tarefa)  # Salva a tarefa no banco de dados
+                tarefa_id = db.obter_tarefas()[-1][0]  # Obtém o ID da última tarefa
+                adicionar_item_tarefa((tarefa_id, tarefa))  # Adiciona a tarefa à lista na interface
+                entrada_tarefa.delete(0, tk.END)
+        else:
+            messagebox.showwarning("Atenção", "Digite uma tarefa.")
+
+    # Função para adicionar item da tarefa
+    def adicionar_item_tarefa(tarefa):
+        tarefa_id = tarefa[0]  # O ID da tarefa
+        descricao_tarefa = tarefa[1]  # A descrição da tarefa
+
+        frame_tarefa = tk.Frame(canvas_interior, bg="White", bd=1, relief=tk.SOLID)
+
+        label_tarefa = tk.Label(frame_tarefa, text=descricao_tarefa, font=("Garamond", 16), bg="White", width=25, height=2, anchor="w")
+        label_tarefa.pack(side=tk.LEFT, fill=tk.X, padx=10, pady=5)
+
+        # Botões de editar e deletar
+        botao_editar = tk.Button(frame_tarefa, image=icon_editar, command=lambda f=frame_tarefa, l=label_tarefa, t_id=tarefa_id: preparar_edicao(f, l, t_id))
+        botao_editar.pack(side=tk.RIGHT, padx=5)
+
+        botao_deletar = tk.Button(frame_tarefa, image=icon_deletar, command=lambda f=frame_tarefa, tarefa_id=tarefa_id: deletar_tarefa(f, tarefa_id))
+        botao_deletar.pack(side=tk.RIGHT, padx=5)
+
+        frame_tarefa.pack(fill=tk.X, padx=5, pady=5)
+        checkbutton = tk.Checkbutton(frame_tarefa, command=lambda label=label_tarefa: alterar_sublinhado(label))
+        checkbutton.pack(side=tk.RIGHT, padx=5)
+
+        canvas_interior.update_idletasks()
+        canvas.config(scrollregion=canvas.bbox("all"))
+
+    # Carregar tarefas do banco de dados ao iniciar
+    def carregar_tarefas():
+        tarefas = db.obter_tarefas()
+        print(f"Tarefas carregadas: {tarefas}")  # Verifique as tarefas carregadas
+        for tarefa in tarefas:
+            adicionar_item_tarefa(tarefa)
+
+    def preparar_edicao(frame_tarefa, label_tarefa, tarefa_id):
+        global frame_em_edicao
+        frame_em_edicao = frame_tarefa
+        entrada_tarefa.delete(0, tk.END)
+        entrada_tarefa.insert(0, label_tarefa.cget("text"))  # Carrega o texto da tarefa no campo de entrada
+
+        # Armazena o ID da tarefa em edição
+        global tarefa_em_edicao
+        tarefa_em_edicao = tarefa_id
+
+    def atualizar_tarefa(nova_tarefa):
+        global frame_em_edicao, tarefa_em_edicao
+        # Atualiza a tarefa no banco de dados
+        db.atualizar_tarefa(tarefa_em_edicao, nova_tarefa)
+
+        for widget in frame_em_edicao.winfo_children():
+            if isinstance(widget, tk.Label):
+                widget.config(text=nova_tarefa)  # Atualiza o texto da tarefa no label
+
+    def alterar_sublinhado(label):
+        fonte_atual = label.cget("font")
+        if "overstrike" in fonte_atual:
+            nova_fonte = fonte_atual.replace(" overstrike", "")
+        else:
+            nova_fonte = fonte_atual + " overstrike"
+        label.config(font=nova_fonte)
+
+    def deletar_tarefa(frame_tarefa, tarefa_id):
+        # Deleta do banco de dados
+        db.deletar_tarefa(tarefa_id)
+        frame_tarefa.destroy()
+        canvas_interior.update_idletasks()
+        canvas.config(scrollregion=canvas.bbox("all"))
+
+    # Conexão com o banco de dados
+    conexao = sqlite3.connect("tarefas.db")
+    cursor = conexao.cursor()
+
+    # Configuração da interface gráfica
+    icon_editar = tk.PhotoImage(file="imgs/edit.png").subsample(15, 15)
+    icon_deletar = tk.PhotoImage(file="imgs/delete.png").subsample(15, 15)
+
+    # Título da aplicação
+    fonte_cabecalho = font.Font(family="Garamond", size=24, weight="bold")
+    rotulo_cabecalho = tk.Label(janela, text="Organizador de tarefas", font=fonte_cabecalho, bg="#F0F0F0", fg="#333333")
+    rotulo_cabecalho.pack(pady=20)
+
+    frame = tk.Frame(janela, bg="#F0F0F0")
+    frame.pack(pady=10)
+
+    entrada_tarefa = tk.Entry(frame, font=("Garamond", 14), relief=tk.FLAT, bg="white", fg="grey", width=30)
+    entrada_tarefa.pack(side=tk.LEFT, padx=10)
+
+    botao_adicionar = tk.Button(frame, command=adicionar_tarefa, text="Adicionar", bg="#4CAF50", fg="white", height=1, width=15, font=("Roboto", 11), relief=tk.FLAT)
+    botao_adicionar.pack(side=tk.LEFT, padx=10)
+
+    frame_lista_tarefas = tk.Frame(janela, bg="#F0F0F0")
+    frame_lista_tarefas.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
+
+    canvas = tk.Canvas(frame_lista_tarefas, bg="White")
+    canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+    scrollbar = tk.Scrollbar(frame_lista_tarefas, orient="vertical", command=canvas.yview)
+    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+    canvas.configure(yscrollcommand=scrollbar.set)
+    canvas_interior = tk.Frame(canvas, bg="White")
+    canvas.create_window((0, 0), window=canvas_interior, anchor="nw")
+    canvas_interior.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+
+    # Inicializa o banco de dados e carrega as tarefas
+    db.criar_tabela()
+    carregar_tarefas()
+
+    janela.mainloop()
+
+
+# Chama a função para exibir a janela de abertura
+janela_abertura()
